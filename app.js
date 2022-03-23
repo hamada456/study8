b9f50a5d20f3cd4b062b75525a0537d2e4465d20
hamada456 <73261938+hamada456@users.noreply.github.com>
2022-03-23 14:55:21 +0900

    18/26

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require("cors");
+const bodyParser = require("body-parser");
+    //const package = require("./package-lock.json");
+    const apiRoot = "/api";
 
-app.get('/', (req, res) => {
-    res.send('hello2222');
-    res.send('hello3333');
-    req.send('hello4444');
+const a = 2;
+const b = 3;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ origin:/http:\/\/localhost/}));
+app.options('*', cors());
+
+const router = express.Router();
+router.get("/", (req, res) => {
+    res.send(`${a}`);
 });
+
+app.use(apiRoot, router);
+
+// app.get('/', (req, res) => {
+//     res.send("a");
+// });
 
 // app.set('/', (req, res) => {
 //     req.json({num:2});
