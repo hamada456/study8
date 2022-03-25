e97ca4deb45060ba31a9d9d812d44f3fc9e6dee4
hamada456 <73261938+hamada456@users.noreply.github.com>
2022-03-25 10:05:43 +0900

    値の表示に成功

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require("cors");
-const os = require("os");
+//const os = require("os");
 //const bodyParser = require("body-parser");
-
-//  http://localhost:3000/numA/3/numB/5
+app.use(cors());
+//http://localhost:3000/numA/3/numB/5
 app.get("/numA/:intA/numB/:intB", (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
     const a = parseInt(req.params.intA);
     const b = parseInt(req.params.intB);
 
     let c = a + b;
     let d = a * b;
 
-    res.json(`足し算：${c}掛け算：${d}`);
+    let objA = new Object();
+
+    objA.num1 = c;
+
+    res.json(objA);
 });
 
 app.listen(PORT, () => console.log(`++++start++++++ ${PORT}`));
--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
     getJSON();
 
-    document.getElementById("plus").textContent = testJ;
+    document.getElementById("plus").textContent = testJ.num1;
 
     function getJSON() {
         // XMLHttpRequest オブジェクトを生成する
-        const req = new XMLHttpRequest();  
+        const req = new XMLHttpRequest();
         // 実際にサーバーへリクエストを送信
         req.open("GET", "http://localhost:3000/numA/3/numB/5",false);
-        //req.send(null);
+        req.send(null);
         // JSON のデータ数分処理、値を返す
         testJ = JSON.parse(req.responseText);
     }
