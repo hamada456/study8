d3160d69a4b0f0a90ed1ab0cee7a7582bbb7d414
hamada456 <73261938+hamada456@users.noreply.github.com>
2022-03-28 17:15:58 +0900

    htmlから数字を取得2

--- index.js
+++ index.js
@@ -7,19 +7,23 @@
     let calculation = document.getElementById('calculation');
 
     calculation.addEventListener('click', function(){
-        let num1 = parseInt(document.getElementById("A").value)
-        let num2 = parseInt(document.getElementById("B").value)
+        let num1 = document.getElementById("A").value
+        let num2 = document.getElementById("B").value
+
+
+        let sendUrl = `http://localhost:3000/study8/app.js/intA/${num1}/intB/${num2}`;
+        //let sendUrl = "http://localhost:3000/study8/app.js/intA/" + num1 + "/intB/" + num2;
 
         // XMLHttpRequest オブジェクトを生成する
         const res = new XMLHttpRequest();
         // 実際にサーバーへリクエストを送信
         res.open("GET", "http://localhost/study8/index.html",false);
-        res.send(null);
+        //res.send(null);
         // JSON のデータ数分処理、値を返す
         res.addEventListener("load", function(){ // loadイベントを登録します。
             console.log(this.response); // 通信結果を出力します。
         }, false);
-        testJ = JSON.parse(res.responseText);
+        //testJ = JSON.parse(res.responseText);
 
     
     // function getJSON() {
