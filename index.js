2655d459ca29057e099c2362ff573e1f26355ad6
hamada456 <73261938+hamada456@users.noreply.github.com>
2022-03-28 14:45:31 +0900

    htmlから数字を取得

--- index.js
+++ index.js
@@ -2,33 +2,36 @@
 
     //getNum();
 
-    getJSON();
+    //getJSON();
 
-    document.getElementById("addition").textContent = testJ.num1;
-    document.getElementById("multiplication").textContent = testJ.num2;
-    //document.getElementById("json").textContent = testJ;
+    let calculation = document.getElementById('calculation');
 
-    // function getNum() {
-    //     // XMLHttpRequest オブジェクトを生成する
-    //     const res = new XMLHttpRequest();
-    //     // 実際にサーバーへリクエストを送信
-    //     res.open("GET", "http://localhost:3000/numA/3/numB/5",false);
-    //     res.send(null);
-    //     // JSON のデータ数分処理、値を返す
-    //     res.addEventListener("load", function(){ // loadイベントを登録します。
-    //         console.log(this.response); // 通信結果を出力します。
-    //     }, false);
-    //     testJ = JSON.parse(res.responseText);
-    // }
-    
-    function getJSON() {
+    calculation.addEventListener('click', function(){
+        let num1 = parseInt(document.getElementById("A").value)
+        let num2 = parseInt(document.getElementById("B").value)
+
         // XMLHttpRequest オブジェクトを生成する
-        const req = new XMLHttpRequest();
+        const res = new XMLHttpRequest();
         // 実際にサーバーへリクエストを送信
-        req.open("GET", "http://localhost:3000/numA/3/numB/5",false);
-        req.send(null);
+        res.open("GET", "http://localhost/study8/index.html",false);
+        res.send(null);
         // JSON のデータ数分処理、値を返す
-        testJ = JSON.parse(req.responseText);
-    }
+        res.addEventListener("load", function(){ // loadイベントを登録します。
+            console.log(this.response); // 通信結果を出力します。
+        }, false);
+        testJ = JSON.parse(res.responseText);
+
+    
+    // function getJSON() {
+    //     // XMLHttpRequest オブジェクトを生成する
+    //     const req = new XMLHttpRequest();
+    //     // 実際にサーバーへリクエストを送信
+    //     req.open("GET", "http://localhost:3000/numA/3/numB/5",false);
+    //     req.send(null);
+    //     // JSON のデータ数分処理、値を返す
+    //     testJ = JSON.parse(req.responseText);
+    // }
+
+    });
 
 })();
